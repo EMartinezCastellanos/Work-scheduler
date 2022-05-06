81bea40832dc495f371f669da1d20a5870cec74a
ErnestoM2014 <1996ernestomartinez@gmail.com>
2022-05-05 22:53:15 -0700

    added comments to outline tasks left for javascript file

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,4 +1,10 @@
-var update = function(){
+var updateTime = function(){
     $('#currentDay').text(moment().format("dddd MMMM, MM YYYY h:mm:ss a"));
 }
-setInterval(update,1000);
+setInterval(updateTime,1000);
+
+//Create code to color code time blocks so that present hour is colored differently from passede hours and future hours 
+
+// Create a function to save Items into the Local storage when save button is pressed 
+
+//Create function to retreive saved LocalStorage items on page Load 
